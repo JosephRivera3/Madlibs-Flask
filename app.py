6cc0328afc6acc6ba25f36135e1e5e024c895935
Joseph <joseph.riveracosta@csedge.org>
2024-07-25 14:40:25 -0700

    update

--- app.py
+++ app.py
@@ -11,11 +11,14 @@ app = Flask(__name__)
 @app.route('/completed', methods = ['GET', 'POST'])
 def completed():
     if request.method == "POST":
-         return render_template("madlib.html", enter="/completed")
-    num1=request.form["Plural noun1"]
-    num2=request.form["Plural noun2"]
+        num1=request.form["Plural noun1"]
+        num2=request.form["Plural noun2"]
+        num3=request.form["1st number"]
+        num4=request.form["plural noun3"]
+        return f"<h1> Thousands of {(num1)} ago, there were calendars that enabled the ancient {(num2)} to divide a year into twelve, each month into {(num3)}, and each week into seven {(num4)}</h1>"
+    return render_template("madlib.html", enter="/completed")
+    
      #return "You Submitted the Madlib!"
-    return f"<h1> Thousands of {(num1)} ago, there were calendars that enabled the ancient {(num2)} to divide a year into twelve</h1>"
-
+    
 if __name__ == '__main__':
         app.run(host='0.0.0.0' , port=80, debug=True)
