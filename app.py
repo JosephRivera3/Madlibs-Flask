b67ec53fa604686f1a76ff78e93ab6847932a27d
Joseph <joseph.riveracosta@csedge.org>
2024-07-25 11:50:52 -0700

    Commit

--- app.py
+++ app.py
@@ -1,25 +1,21 @@
-from flask import Flask, render_template, request, url_for
-import random
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/madlib', methods = ['GET', 'POST'])
-
-def fill_in_blank():
+def Starter():
     if request.method == "POST":
         return render_template("madlib.html", enter="/completed")
-    #Pluralnoun2=pn2
 
-    #retUrn render_template(flex1="/input")
 
 @app.route('/completed', methods = ['GET', 'POST'])
 def completed():
     if request.method == "POST":
-        num1=request.form["Plural noun1"]
+        return render_template("madlib.html")
+    num1=request.form["Plural noun1"]
     #num2=request.form["Plural noun2"]
     # #return "You Submitted the Madlib!"
-        return render_template("madlib.html")
-    return f"<h1> Thousands of {url_for(num1)}ago </h1>"
+    return f"<h1> Thousands of {(num1)} ago </h1>"
 
 if __name__ == '__main__':
         app.run(host='0.0.0.0' , port=80, debug=True)
